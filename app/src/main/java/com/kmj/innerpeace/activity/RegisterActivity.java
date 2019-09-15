d1bcf8d719b23230744076c3bb0f1a8b8a82f09f
unknown <minjun3021@naver.com>
2019-09-16 03:55:08 +0900

    register

--- app/src/main/java/com/kmj/innerpeace/activity/RegisterActivity.java
+++ app/src/main/java/com/kmj/innerpeace/activity/RegisterActivity.java
@@ -1,15 +1,80 @@
 package com.kmj.innerpeace.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.kmj.innerpeace.R;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    EditText id,pw,name,email,phone;
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        setup();
+    }
+    void setup(){
+        Window window = getWindow();
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#121319"));
+
+        id=findViewById(R.id.register_id);
+        pw=findViewById(R.id.register_pw);
+        name=findViewById(R.id.register_name);
+        email=findViewById(R.id.register_email);
+        phone=findViewById(R.id.register_phonenumber);
+        register=findViewById(R.id.register_btn);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!id.getText().toString().replace(" ", "").equals("")) {
+                    if (!pw.getText().toString().replace(" ", "").equals("")) {
+                        if (!name.getText().toString().replace(" ", "").equals("")) {
+                            if (!email.getText().toString().replace(" ", "").equals("")) {
+                                if (!phone.getText().toString().replace(" ", "").equals("")) {
+                                    //TODO retrofit
+                                    tryRegister();
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "phone number를 입력하세요.", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "email를 입력하세요.", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "name를 입력하세요.", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "pw를 입력하세요.", Toast.LENGTH_SHORT).show();
+                    }
+
+                } else {
+                    Toast.makeText(RegisterActivity.this, "id를 입력하세요.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        phone.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    tryRegister();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+    void tryRegister() {
+        Toast.makeText(this, "Wait", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/kmj/innerpeace/activity/SplashActivity.java
+++ app/src/main/java/com/kmj/innerpeace/activity/SplashActivity.java
@@ -1,9 +1,12 @@
 package com.kmj.innerpeace.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.kmj.innerpeace.R;
 
@@ -13,6 +16,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setup();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -23,5 +27,10 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, 2000);
     }
+    void setup() {
+        Window window = getWindow();
 
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.parseColor("#121319"));
+    }
 }
