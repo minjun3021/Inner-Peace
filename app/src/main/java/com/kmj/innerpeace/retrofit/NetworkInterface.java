c2036fb1433552d0c6caa1f8291f3c5ff40a20bc
unknown <minjun3021@naver.com>
2019-09-27 08:04:59 +0900

    realrealrealfinish

--- app/src/main/java/com/kmj/innerpeace/retrofit/NetworkInterface.java
+++ app/src/main/java/com/kmj/innerpeace/retrofit/NetworkInterface.java
@@ -21,30 +21,6 @@ import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface NetworkInterface {
-
-//    @POST("/user/register")
-//    @FormUrlEncoded
-//    Call<RegisterModel> Register(@Field("name") String name, @Field("facebookId") String facebookId, @Field("profileURL") String profileURL);
-//
-//    @POST("/user/start")
-//    @FormUrlEncoded
-//    Call<StartModel> Start(@Field("token") String token, @Field("current") String current);
-//
-//    @POST("/user/end")
-//    @FormUrlEncoded
-//    Call<EndModel> End(@Field("token") String token);
-//
-//    @POST("/user/ranking")
-//    Call<ArrayList<UserModel>> Ranking();
-//
-//    @POST("/user/friend/ranking")
-//    @FormUrlEncoded
-//    Call<ArrayList<UserModel>> FriendsRanking(@Field("ids") String ids);
-//
-//    @POST("/user/record")
-//    @FormUrlEncoded
-//    Call<ArrayList<RecordModel>> Record(@Field("token") String token);
-
     @POST("/save")
     Call<SaveRes> egg(@Body SendData sendData);
 
